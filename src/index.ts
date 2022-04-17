c10d6706a1a3a13301d02c6b6c725c881d08d059
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-04-17 23:36:03 +1200

    Update default exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import Puppeteer from "./external/puppeteer";
 import Pug from "./external/pug";
 
 export default buildMakePapyrus({ Browser: Puppeteer, TemplatingEngine: Pug });
+
+export { Papyrus, PapyrusConfiguration } from "./entities/papyrus/papyrus";
